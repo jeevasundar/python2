5763c2c7bd1d2c7b4574cc29baf4c3372f8c85d8
jeevasundar <86206633+jeevasundar@users.noreply.github.com>
2022-10-30 11:26:48 +0530

    Add files via upload

---
+++ prog.py
@@ -0,0 +1,28 @@
+
+m = str(input("enter name of month: "))
+if m =='january':
+    print("january-31")
+elif m == "february":
+    print('february-28/29')
+elif m =='march':
+    print('march-31')
+elif m == "april":
+    print('april-30')
+elif m == "may":
+    print('may-31')
+elif m == "june":
+    print('june-30')
+elif m == 'july':
+    print('july-31')
+elif m == "august":
+    print('august-31')
+elif m == "september":
+    print('september-30') w
+elif m =="october":
+    print('october-31')
+elif m == "november":
+    print('november-30')
+elif m == "december":
+    print('december-31')
+else:
+    print("invalid")
